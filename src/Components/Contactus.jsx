31bb32e9ddb0fcbf75b90f06109c9484c4762346
ikennaex <ikennaexcel@gmail.com>
2024-04-16 11:49:41 +0100

    update address

--- src/Components/Contactus.jsx
+++ src/Components/Contactus.jsx
@@ -49,17 +49,15 @@ const Contactus = () => {
           United Kingdom
         </p>
 
-        {/* <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2496.9677997929257!2d-1.0977435241622517!3d51.25650222900223!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48742227bc330d19%3A0x7de7083012f60423!2s134%20Culver%20Rd%2C%20Basingstoke%20RG21%203NL%2C%20UK!5e0!3m2!1sen!2sng!4v1713253645657!5m2!1sen!2sng"
-          style={{
-            width:"600",
-            height:"450",
-            // style:"border:0;
-            // all:"",
-            loading:"lazy",
-            referrerpolicy:"no-referrer-when-downgrade"
-          }}
-        ></iframe> */}
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2496.967980599574!2d-1.0951686!3d51.2564989!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48742227bc330d19%3A0x7de7083012f60423!2s134%20Culver%20Rd%2C%20Basingstoke%20RG21%203NL%2C%20UK!5e0!3m2!1sen!2sng!4v1713264166975!5m2!1sen!2sng"
+          width="600"
+          height="450"
+          style={{ border: "0" }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
         <p>Damiscakes@outlook.com </p>
       </div>
     </div>
